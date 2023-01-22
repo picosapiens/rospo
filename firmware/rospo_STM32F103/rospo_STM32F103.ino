2154007b378563c2734f45b6799d306cd6f569d1
picosapiens <99155710+picosapiens@users.noreply.github.com>
2023-01-21 22:18:01 -0500

    GPL license
    Adding GPL license headers to the code

--- firmware/rospo_STM32F103/rospo_STM32F103.ino
+++ firmware/rospo_STM32F103/rospo_STM32F103.ino
@@ -1,3 +1,24 @@
+//-----------------------------------------------------------------------------
+// rospo_STM32F103.ino
+//-----------------------------------------------------------------------------
+//
+// This file is part of Rospo.
+//
+//  Rospo is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Rospo is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Rospo.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+
 #include "libmaple/dma.h"
 
 // BUFFER SIZE MUST BE A POWER OF TWO
